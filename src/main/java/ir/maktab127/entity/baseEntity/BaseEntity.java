dcc1d49c78daa6a7bd8ce88244e7b76096654051
Majid Qeisari <majidq77@yahoo.com>
2025-07-08 17:15:30 +0330

    extend base entity

--- src/main/java/ir/maktab127/entity/baseEntity/BaseEntity.java
+++ src/main/java/ir/maktab127/entity/baseEntity/BaseEntity.java
@@ -3,19 +3,36 @@ package ir.maktab127.entity.baseEntity;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
 
 import java.io.Serializable;
 import java.time.LocalDateTime;
+import java.time.ZonedDateTime;
 
-@Entity
+
 @Getter
 @Setter
-public class BaseEntity<ID extends Serializable> implements Serializable {
+@MappedSuperclass
+public abstract class BaseEntity<ID extends Serializable> implements Serializable {
     @Id
     @GeneratedValue(strategy = GenerationType.SEQUENCE)
     private Long id;
 
-    @Column(nullable = false)
-    private LocalDateTime createdAt;
+    @Column(name = "CREATE_DATE")
+    @CreatedDate
+    private ZonedDateTime createDate;
+
+    @Column(name = "LAST_UPDATE_DATE")
+    @LastModifiedDate
+    private ZonedDateTime lastUpdateDate;
+
+
+
+    @PrePersist
+    public void registrationDate() {
+        createDate = ZonedDateTime.now();
+
+    }
 
 }
